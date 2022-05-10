94d80656726a190913920966e4f0d500884bbde0
Terry Nguyen <terry@terrehbyte.com>
2022-05-09 17:47:22 -0700

    GameStateManager handles warmup/in-prog/gameover

---
+++ Assets/SurvivalShooter/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class GameStateManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;       // Reference to the player's health.
+    
+    public Animator hudAnimator;       // Reference to the animator component.
+
+    public float startDelay = 1.0f;
+    public float gameOverDelay = 1.0f;
+    public float restartDelay = 3.0f;
+
+    private float transitionDelay = 0.0f;
+
+    private int sceneBuildIndex;
+
+    public enum GameState
+    {
+        None,
+        Warmup,
+        InProgress,
+        End
+    }
+    public GameState CurrentGameState { get; private set; }
+    
+    [SerializeField]
+    private EnemyManager[] enemyManagers;
+
+    void ToGameState(GameState newState)
+    {
+        // exit current state - clean-up
+        Debug.Log("Exiting game state: " + CurrentGameState);
+        switch (CurrentGameState)
+        {
+            case GameState.None:
+                // left blank intentionally
+                break;
+            case GameState.Warmup:
+                break;
+            case GameState.InProgress:
+                // turn on all of the spawners
+                foreach (var spawner in enemyManagers) { spawner.enabled = false; }
+                break;
+            case GameState.End:
+                break;
+            default:
+                Debug.LogError("Unhandled exit from game state: " + CurrentGameState);
+                break;
+        }
+        
+        // enter new state - clean-up
+        Debug.Log("Entering game state: " + newState);
+        switch (newState)
+        {
+            case GameState.None:
+                // left blank intentionally
+                break;
+            case GameState.Warmup:
+                transitionDelay = startDelay;
+                break;
+            case GameState.InProgress:
+                // turn on all of the spawners
+                foreach (var spawner in enemyManagers) { spawner.enabled = true; }
+                transitionDelay = gameOverDelay;
+                break;
+            case GameState.End:
+                // ... tell the animator the game is over.
+                hudAnimator.SetTrigger ("GameOver");
+                transitionDelay = restartDelay;
+                break;
+            default:
+                Debug.LogError("Unhandled entry to game state: " + CurrentGameState);
+                break;
+        }
+
+        CurrentGameState = newState;
+    }
+    
+    void Start()
+    {
+        sceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        ToGameState(GameState.Warmup);
+    }
+
+    void Update ()
+    {
+        var nextState = CurrentGameState;
+        switch (CurrentGameState)
+        {
+            case GameState.Warmup:
+                transitionDelay -= Time.deltaTime;
+                if (transitionDelay <= 0.0f)
+                {
+                    nextState = GameState.InProgress;
+                }
+                break;
+            case GameState.InProgress:
+                if (playerHealth.currentHealth <= 0)
+                {
+                    transitionDelay -= Time.deltaTime;
+                    if (transitionDelay <= 0.0f)
+                    {
+                        nextState = GameState.End;
+                    }
+                }
+                break;
+            case GameState.End:
+                transitionDelay -= Time.deltaTime;
+                if(transitionDelay <= 0.0f)
+                {
+                    RestartLevel();
+                }
+                break;
+            default:
+                break;
+        }
+
+        // is a transition needed?
+        if (nextState != CurrentGameState)
+        {
+            ToGameState(nextState);
+        }
+    }
+    
+    public void RestartLevel ()
+    {
+        // Reload the level that is currently loaded.
+        SceneManager.LoadScene (sceneBuildIndex);
+    }
+}
--- Assets/SurvivalShooter/Scripts/Player/PlayerHealth.cs
+++ Assets/SurvivalShooter/Scripts/Player/PlayerHealth.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
@@ -7,34 +8,33 @@ public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth = 100;                            // The amount of health the player starts the game with.
     public int currentHealth;                                   // The current health the player has.
-    public Slider healthSlider;                                 // Reference to the UI's health bar.
-    public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
-    public AudioClip deathClip;                                 // The audio clip to play when the player dies.
+    [SerializeField]
+    private Slider healthSlider;                                 // Reference to the UI's health bar.
+    [SerializeField]
+    private Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+    [SerializeField]
+    private AudioClip deathClip;                                 // The audio clip to play when the player dies.
     public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
-
-
-    Animator anim;                                              // Reference to the Animator component.
-    AudioSource playerAudio;                                    // Reference to the AudioSource component.
-    PlayerMovement playerMovement;                              // Reference to the player's movement.
-    PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
-    bool isDead;                                                // Whether the player is dead.
-    bool damaged;                                               // True when the player gets damaged.
-
-
-    void Awake ()
+    
+    [SerializeField]
+    private Animator anim;                                              // Reference to the Animator component.
+    [SerializeField]
+    private AudioSource playerAudio;                                    // Reference to the AudioSource component.
+    [SerializeField]
+    private PlayerMovement playerMovement;                              // Reference to the player's movement.
+    [SerializeField]
+    private PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
+    
+    private bool isDead;                                                // Whether the player is dead.
+    private bool damaged;                                               // True when the player gets damaged.
+    
+    void Start()
     {
-        // Setting up the references.
-        anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
-        playerMovement = GetComponent <PlayerMovement> ();
-        playerShooting = GetComponentInChildren <PlayerShooting> ();
-
         // Set the initial health of the player.
         currentHealth = startingHealth;
     }
-
-
+    
     void Update ()
     {
         // If the player has just been damaged...
@@ -96,12 +96,5 @@ public class PlayerHealth : MonoBehaviour
         // Turn off the movement and shooting scripts.
         playerMovement.enabled = false;
         playerShooting.enabled = false;
-    }
-
-
-    public void RestartLevel ()
-    {
-        // Reload the level that is currently loaded.
-        SceneManager.LoadScene (0);
     }
 }
