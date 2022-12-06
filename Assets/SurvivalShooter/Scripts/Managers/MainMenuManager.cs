9c4ec89f727c4aa84bfe99ad6dfa613e838581ef
Terry Nguyen <terry@terrehbyte.com>
2022-12-06 15:44:35 -0800

    Add UniTask and UI transitions + fix pause menu

--- Assets/SurvivalShooter/Scripts/Managers/MainMenuManager.cs
+++ Assets/SurvivalShooter/Scripts/Managers/MainMenuManager.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+using Cysharp.Threading.Tasks;
+
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -21,6 +26,8 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField]
     private GameObject settingsPanel;
 
+    private CancellationTokenSource transitionCancellationSource;
+
     public enum Menus
     {
         None,
@@ -30,7 +37,7 @@ public class MainMenuManager : MonoBehaviour
     public Menus CurrentMenu { get; private set; }
     private GameObject lastShownMenu;
 
-    public void ShowMenu(Menus menu)
+    public async void ShowMenu(Menus menu)
     {
         // do nothing if we are trying to show the currently shown menu
         if(menu == CurrentMenu) {return;}
@@ -63,6 +70,20 @@ public class MainMenuManager : MonoBehaviour
 
         lastShownMenu = newMenu;
         CurrentMenu = menu;
+
+        // transition if group is available
+        if (transitionCancellationSource.Token != null)
+        {
+            transitionCancellationSource.Cancel();
+            transitionCancellationSource.Dispose();
+            transitionCancellationSource = new CancellationTokenSource();
+        }
+
+        if (newMenu.TryGetComponent<CanvasGroup>(out var newGroup))
+        {
+            newGroup.alpha = 0.0f;
+            await DoAlphaTransitionAsync(newMenu.GetComponent<CanvasGroup>(), 1.0f, 0.3f, transitionCancellationSource.Token);
+        }
     }
 
     public void ShowMenu(int menuEnumId)
@@ -80,8 +101,37 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    private async Task DoAlphaTransitionAsync(CanvasGroup group, float targetAlpha, float duration, CancellationToken token)
+    {
+        float startAlpha = group.alpha;
+        float timer = 0.0f;
+        while(timer < duration)
+        {
+            float progress = timer / duration;
+
+            group.alpha = Mathf.Lerp(startAlpha, targetAlpha, progress);
+
+            timer += Time.deltaTime;
+
+            await UniTask.Yield(token);
+        }
+
+        group.alpha = targetAlpha;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void OnEnable()
+    {
+        transitionCancellationSource = new CancellationTokenSource();
+    }
+
+    private void OnDisable()
+    {
+        transitionCancellationSource.Cancel();
+        transitionCancellationSource.Dispose();
     }
 }
