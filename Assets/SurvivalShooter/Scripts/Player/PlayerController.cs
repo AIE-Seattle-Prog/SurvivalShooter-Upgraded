acf612bd31df322b2849577bb664361c831a252d
Terry Nguyen <terry@terrehbyte.com>
2023-03-24 10:18:46 -0700

    Prevent NaN rotations and fix refs

--- Assets/SurvivalShooter/Scripts/Player/PlayerController.cs
+++ Assets/SurvivalShooter/Scripts/Player/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     [Space]
     public PlayerInput input;
 
+    public float shootPlaneOffset = 0.0f;
+    public Transform gunOffset;
+
     [Header("Mouse Settings")]
     public LayerMask floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
     public float camRayLength = 100f;          // The length of the ray from the camera into the scene.
@@ -81,11 +84,27 @@ public class PlayerController : MonoBehaviour
         // Ensure the vector is entirely along the floor plane.
         playerToMouse.y = 0f;
 
+        // Offset rotation to account for distance between body and gun
+        float aimOffset = (gunOffset.position - transform.position).magnitude;
+        float distToShoot = playerToMouse.magnitude;
+        float turnOffset = Mathf.Rad2Deg * Mathf.Asin(aimOffset / distToShoot);
+
+        // fallback to excluding offset if invalid
+        if(float.IsNaN(turnOffset)) { turnOffset = 0f; }
+
         // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-        Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+        Quaternion newRotation = Quaternion.LookRotation(playerToMouse) * Quaternion.AngleAxis(-turnOffset, Vector3.up);
 
         // Pass to movement component
         movement.SetMoveRotation(newRotation);
+
+        // Vector3 debugHitPoint = hitPoint + Vector3.up * 0.3f;
+
+        // Debug.DrawRay(shoot.gunTipTransform.position, shoot.gunTipTransform.forward * 10.0f, Color.yellow);
+
+        // Debug.DrawRay(debugHitPoint, Vector3.up * 3.0f, Color.red);
+        // Debug.DrawRay(debugHitPoint - Vector3.right * 0.1f, Vector3.right * 0.2f, Color.red);
+        // Debug.DrawRay(debugHitPoint - Vector3.forward * 0.1f, Vector3.forward * 0.2f, Color.red);
     }
 
     private void HandlePause(CallbackContext obj)
@@ -107,5 +126,16 @@ public class PlayerController : MonoBehaviour
     public void HandleMoveVertical(CallbackContext callbackContext)
     {
         movement.SetMoveForward(callbackContext.ReadValue<float>());
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+
+        Vector3 debugOrigin = transform.position;
+        debugOrigin.y = shootPlaneOffset;
+
+        Gizmos.DrawRay(debugOrigin, Vector3.forward * 2.0f);
+        Gizmos.DrawRay(debugOrigin, Vector3.right * 2.0f);
     }
 }
