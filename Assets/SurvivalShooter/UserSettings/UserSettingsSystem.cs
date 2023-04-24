1e9d793c486417a1611f42efaf52c484115fad2d
Terry Nguyen <terry@terrehbyte.com>
2023-04-24 12:13:02 -0700

    Clean-up extra files and stub missing files

---
+++ Assets/SurvivalShooter/UserSettings/UserSettingsSystem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UserSettingsSystem
+{
+    public static float SoundLevel { get; set; } = -10; 
+    public static float MusicLevel { get; set; } = -10; 
+    public static int GraphicsPreset { get; set; } = 2;
+}
